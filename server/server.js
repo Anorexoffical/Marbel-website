79ac41e43df46638e9749e590091a902e20e2ab7
Anorex <anorexoffical@gmail.com>
2026-01-03 16:05:42 +0500

    server updated

--- server/server.js
+++ server/server.js
@@ -1,37 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 // Routers
 const blogRoutes = require("./routes/blogRoutes");
 const userRoutes = require("./routes/userRoutes");
-const path = require("path");
+
 const app = express();
 const PORT = 3001;
+
+// Upload folder
 const uploadDir = path.join(__dirname, "uploads");
+
 // Middleware
 app.use(cors({
-  origin: ["https://marble-domain","http://localhost:3000", "http://localhost:5173"], // Allow only your production domain
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true
+  origin: [
+    "http://localhost:5173",               // local frontend
+    "https://www.wahatalhijazmarble.com"  // production frontend
+  ],
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(uploadDir));
 
-// Database Connection
-mongoose
-  .connect("mongodb://127.0.0.1:27017/marble")
+// Database connection
+mongoose.connect("mongodb://127.0.0.1:27017/marble")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
-
 // Mount routers
- app.use('/api/blogs', blogRoutes);
- app.use('/api/user', userRoutes);
+app.use('/api/blogs', blogRoutes);
+app.use('/api/user', userRoutes);
 
-
-// Start the server (No SSL, since it will be handled by Nginx or a reverse proxy)
+// Start server
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
